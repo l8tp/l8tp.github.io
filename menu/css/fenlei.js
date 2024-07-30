90f712d402221dfc783ba8d5458b3186311b621e
l8tp <64537212+l8tp@users.noreply.github.com>
2024-07-30 19:17:54 +0800

    更改导航栏样式

--- menu/css/fenlei.js
+++ menu/css/fenlei.js
@@ -1,12 +1,12 @@
-var pathname = window.location.pathname; // 使用window.location.pathname获取当前页面的路径部分
+﻿var pathname = window.location.pathname; // 使用window.location.pathname获取当前页面的路径部分
 if (pathname.endsWith('/')) { pathname = pathname.slice(0, -1);} // 如果路径以斜杠结尾，则去除它
 var parentDirectoryName = pathname.split('/').slice(-2, -1)[0]; // 使用字符串的split方法和正则表达式来获取上级目录的名称
-console.log(parentDirectoryName); // 输出上级目录的名称
-
-//以上用来获取上级目录的名称
+//以上用来获取上级文件夹的名称
 
 var XML = '../fen-lei.xml' //定义xml的路径
-if("menu" == parentDirectoryName){XML = 'fen-lei.xml'} //判断是否为首页
+var RootPath = false; //定义不是首页
+if("menu" == parentDirectoryName){XML = 'fen-lei.xml'; RootPath = true} //判断当前是否为首页
+
 
 const FenLeiContainer = document.querySelector('.FenLei');
 //const声明常量，但其内部属性和元素可以改变
@@ -22,10 +22,9 @@ fetch(XML)
 		links.forEach(link => {  //遍历每一个link
 			const url = link.querySelector('url').textContent.toLowerCase();  //选择标签内的文本内容
 			const content = link.querySelector('content').textContent.toLowerCase();
-			
 			const li = document.createElement('li');  //动态创建一个空白列表
 			li.className = 'aurl';  //设置列表属性
-			if("menu" == parentDirectoryName){ //判断是否为首页
+			if(RootPath == true){ //判断当前是否为首页
 				li.innerHTML = `<a href="${url}" target="_top" rel="external nofollow"  data-lastfrom="" title="">${content}</a>`;
 					 //设置列表内容。反引号代表模板字符串
 			}else{
@@ -33,12 +32,14 @@ fetch(XML)
 					 //设置列表内容。反引号代表模板字符串
 			}
 			FenLeiContainer.appendChild(li);  //选择并添加子节点
+			//以上用来显示导航按钮
 			
-			if(url.includes("/")){ //判断是否为首页
+			if(url.includes("/")){ //判断是否为首页按钮
 				if (url.split('/').slice(-2, -1)[0] == parentDirectoryName ) {li.className = 'Active';} //设置当前列表属性
 			}else{
-				if ("menu" == parentDirectoryName ) {li.className = 'Active';} //设置当前列表属性
+				if (RootPath == true ) {li.className = 'Active';} //设置当前列表属性
 			}
+			//以上用来根据上级文件夹名字判断是否为当前按钮，并突出显示
 			
 		});
 
